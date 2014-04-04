87234b8b5b987d2076b007a839cbf14bafd7b3c2
Luis Doubrava <luis@cg.nl>
2014-04-04 10:34:16 +0200

    add belongs_to product to suggestions

--- app/models/spree/suggestion.rb
+++ app/models/spree/suggestion.rb
@@ -1,12 +1,12 @@
 class Spree::Suggestion < ActiveRecord::Base
   validates :keywords, presence: true
+  belongs_to :product
 
   scope :has_data, -> { where ["data IS NOT ?", nil] }
 
   def self.relevant(term)
     config = Spree::Autosuggest::Config
-
-    select([:keywords, :data]).
+    select('*').
       where("count >= ?", config.min_count).
       where("items_found != 0").
       where("keywords LIKE ? OR keywords LIKE ?", term + '%', term + '%').
